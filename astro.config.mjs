5eee8d4ed876afb50d89cca37fd9cbd154a2a9d9
uthwik <uthwik.reddy.k@gmail.com>
2026-08-09 17:42:23 +0530

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://uthwik.github.io/rtl-design-challenge',
+  base: '/rtl-design-challenge',
+});
